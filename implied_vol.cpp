bf0c1ed77c5d2e9faad916673d792f6796c68443
jackedison <jack.edison95@gmail.com>
2020-11-29 21:21:24 +0100

    refactored to a class

--- implied_vol.cpp
+++ implied_vol.cpp
@@ -5,39 +5,61 @@
 
 using namespace std;
 
-double calc_d1(double s, double k, double r, double t, double v, double d){
-    double d1 = (log(s/k) + (r + pow(v, 2)/2) * t) / (v * sqrt(t));
-    return d1;
-}
+class Option
+{
+    // Access specifier for public variables and member functions (can also have protected and private)
+    public:
+      double s, k, r, t, v, d;
 
-double calc_d2(double d1, double t, double v){
-    double d2 = d1 - v * sqrt(t);
-    return d2;
-}
+    // Parametrised Constructor (default one can be without variables if allowed). Always public and no return value.
+    Option(double spot_price, double strike_price, double risk_free_rate, double time_to_expiration, double volatility, double dividend_yield = 0)
+    {
+        // Parameters: s Stock Price, k Strike price, r Risk free rate, t Time to maturity (years), v Volatility, d Dividend yield = 0
+        s = spot_price;
+        k = strike_price;
+        r = risk_free_rate;
+        t = time_to_expiration;
+        v = volatility;
+        d = dividend_yield;
+    }
 
-double n(double x) { 
-    /* CDF of the normal distribution; Phi(-∞, x) aka N(x)
-    i.e. if normally distributed, what % does x standard deviations cover. So 0 standard deviations = 50% (below mean). 1std = 84.13%, -1std = 15.87%. 1.96std = 97.5% */
-    return erfc(-x/sqrt(2))/2;
-}
+    double call_price()
+    {
+        // Returns: Call price in $
+        return n(d1()) * s - n(d2()) * k * pow(exp(1), -r * t);
+    }
 
-double call_price(double s, double k, double r, double t, double v, double d = 0){
-    // Parameters: s Stock Price, k Strike price, r Risk free rate, t Time to maturity (years), v Volatility, d Dividend yield = 0
-    // Returns: Call price in $
-    double d1 = calc_d1(s, k, r, t, v, d);
-    double d2 = calc_d2(d1, t, v);
-    double call_px = n(d1) * s - n(d2) * k * pow(exp(1), -r * t);
-    return call_px;
-}
+    double put_price()
+    {
+        // Returns: Put price in $
+        return k * pow(exp(1), -r * t) * n(-d2()) - s * pow(exp(1), -d * t) * n(-d1());
+    }
 
-double put_price(double s, double k, double r, double t, double v, double d = 0){
-    // Parameters: s Stock Price, k Strike price, r Risk free rate, t Time to maturity (years), v Volatility, d Dividend yield = 0
-    // Returns: Call price in $
-    double d1 = calc_d1(s, k, r, t, v, d);
-    double d2 = calc_d2(d1, t, v);
-    double put_px = k * pow(exp(1), -r * t) * n(-d2) - s * pow(exp(1), -d * t) * n(-d1);
-    return put_px;
-}
+    void print()
+    {
+        cout << d2() << endl;
+    }
+
+    // Access specifier for private variables and member functions
+    private:
+        double d1()
+        {
+            return (log(s/k) + (r + pow(v, 2)/2) * t) / (v * sqrt(t));
+        }
+
+        double d2()
+        {
+            double d2 = d1() - v * sqrt(t);
+            return d2;
+        }
+
+        double n(double x)
+        { 
+            /* CDF of the normal distribution; Phi(-∞, x) aka N(x)
+            i.e. if normally distributed, what % does x standard deviations cover. So 0 standard deviations = 50% (below mean). 1std = 84.13%, -1std = 15.87%. 1.96std = 97.5% */
+            return erfc(-x/sqrt(2))/2;
+        }
+};
 
 int main(){
     // Input parameters
@@ -49,12 +71,10 @@ int main(){
     double volatility = 0.15;  // to calculate theoretical, if not will compute IV
 
     // Black-Scholes calculations
-    double call_px = call_price(spot_price, strike_price, risk_free_rate, time_to_expiration, volatility, dividend_yield);
-    double put_px = put_price(spot_price, strike_price, risk_free_rate, time_to_expiration, volatility, dividend_yield);
+    Option option(spot_price, strike_price, risk_free_rate, time_to_expiration, volatility, dividend_yield);
+    cout << "Call price: $" << option.call_price() << endl;
+    cout << "Put price: $" << option.put_price() << endl;
 
-
-    cout << "Call price: $" << call_px << endl;
-    cout << "Put price: $" << put_px << endl;
     return 1;
 }
 
